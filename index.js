935da6dbb0d34be00bedd23574b2c251f0004691
Adilson Pinheiro de Carvalho Junior <79886778+Adilson-Junior-ADS@users.noreply.github.com>
2021-03-22 19:03:19 -0400

    Add files via upload
    aula de javascript funções

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(
+    function(){
+        alert("O documento carregou")
+    }
+)
+
+
+
+$("#inicio").click(
+    function() {
+        alert("Opa, você clicou no botão inicio")
+    }
+)
+
+$("#cadastro").click(
+    function() {
+        alert("haha vai se cadastrar vagabundo")
+        
+    }
+)
